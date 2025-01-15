e5a9e351b4427746c0b4baa47076d87a3ee9e3d7
narin45446 <narin45446@gmail.com>
2025-01-15 16:58:26 +0900

    김나린-MovieDAO, ojdbc8.jar 추가, 패키지 생성

---
+++ JavaMiniProject/src/com/sist/dao/MovieDAO.java
@@ -0,0 +1,62 @@
+package com.sist.dao;
+import com.sist.main.*;
+import java.sql.*;
+
+public class MovieDAO {
+	private Connection conn;
+	private PreparedStatement ps;
+	private final String URL="jdbc:oracle:thin:@211.238.142.124:1521:XE";
+	private static MovieDAO dao;
+	
+	public MovieDAO() {
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+		} catch (Exception e) {
+		}
+	}
+	
+	public static MovieDAO newInstance() {
+		if(dao==null) dao=new MovieDAO();
+		return dao;
+	}
+	
+	public void getConnection() {
+		try {
+			conn=DriverManager.getConnection(URL,"hr_2","happy");
+		} catch (Exception e) {
+		}
+	}
+	
+	public void disConnection() {
+		try {
+			if(ps!=null) ps.close();
+			if(conn!=null) conn.close();
+		} catch (Exception e) {
+		}
+	}
+	
+//	기능
+	public void MovieInsert(MovieVO vo) {
+		try {
+			getConnection();
+			String sql="INSERT INTO movie VALUES(movie_mno_seq.nextval,"
+					+ "?,?,?,?,?,?,sysdate,?,?,?,0,?,?)";
+			ps=conn.prepareStatement(sql);
+			ps.setString(1, vo.getM_title());//title
+			ps.setString(2, vo.getM_eng_title());//eng title
+			ps.setString(3, vo.getM_post());//post
+			ps.setString(4, vo.getNation());//nation
+			ps.setString(5, vo.getGenre());//genre
+			ps.setString(6, vo.getRuntime());//runtime
+			ps.setInt(7, vo.getTotal_audi());//total audi
+			ps.setString(8, vo.getDir());//dir
+			ps.setString(9, vo.getAct());//act
+			ps.setString(10, vo.getStory());//story
+			ps.setString(11, vo.getGrade());//grade
+			ps.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			disConnection();
+		}
+	}
