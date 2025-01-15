dfb90c81c810808990aca988d42c318ba1a6ddff
wnstn <wnstn@kimjunsu>
2025-01-15 16:27:46 +0900

    무비 클래스

---
+++ JavaMiniProject/src/com/sist/main/Movie.java
@@ -0,0 +1,22 @@
+package com.sist.main;
+
+import java.util.Date;
+
+public class Movie {
+	
+		private String m_title;
+		private String m_eng_title;
+		private String m_post;
+		private String nation;
+		private String genre;
+		private String runtime;
+		private Date reg_date;
+		private int total_audi;
+		private String dir;
+		private String act;
+		private Double raiting;
+		private String story;
+		private String grade;
+
+	}
+
