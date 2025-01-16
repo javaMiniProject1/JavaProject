df5c253584aedcd16110b1c85447ba607364a854
wnstn <wnstn@kimjunsu>
2025-01-16 15:29:30 +0900

    dao커밋

--- JavaMiniProject/src/com/sist/dao/MemberDAO.java
+++ JavaMiniProject/src/com/sist/dao/MemberDAO.java
@@ -92,6 +92,69 @@ public class MemberDAO {
 		
 		return vo;
 	}
+	public MemberVO isLogin(String id,String pwd)
+	   {
+		   MemberVO vo=new MemberVO();
+		   try
+		   {
+			   // 1. 연결 
+			   getConnection();
+			   // 2. SQL문장 전송 => id존재여부 확인 
+			   String sql="SELECT COUNT(*) FROM member "
+					     +"WHERE id=?";
+			   ps=conn.prepareStatement(sql);
+			   ps.setString(1, id);
+			   
+			   // 실행 요청 
+			   ResultSet rs=ps.executeQuery();
+			   rs.next();
+			   int count=rs.getInt(1);
+			   rs.close();
+			   
+			   if(count==0) // ID없는 상태 
+			   {
+				   vo.setMsg("NOID");
+			   } 
+			   else // ID있는 상태
+			   {
+				   // 비밀번호 확인 
+				   sql="SELECT id,pwd,name,sex "
+					  +"FROM member "
+					  +"WHERE id=?";
+				   ps=conn.prepareStatement(sql);
+				   // 실행전에 ?에 값을 채운다 
+				   ps.setString(1, id);
+				   // 결과값 
+				   rs=ps.executeQuery();
+				   rs.next(); // 한줄씩 읽어 온다 
+				   //            ----- RECORD
+				   vo.setId(rs.getString(1));
+				   vo.setName(rs.getString(3));
+				   vo.setSex(rs.getString(4));
+				   String db_pwd=rs.getString(2);
+				   
+				   // 비밀번호 검사 
+				   if(db_pwd.equals(pwd)) // 로그인
+				   {
+					   vo.setMsg("OK");
+				   }
+				   else // 비밀번호가 틀리다 
+				   {
+					   vo.setMsg("NOPWD");
+				   }
+				   
+			   }
+			   
+		   }catch(Exception ex)
+		   {
+			   ex.printStackTrace();
+		   }
+		   finally
+		   {
+			   disconnetion();
+		   }
+		   return vo;
+	   }
 	/*
 	public ArrayList<String> getMemberId(MemberVO vo) { // UI 에서 연결할지 고민해야 함
 		ArrayList<String> connUsers = new ArrayList<String>();
